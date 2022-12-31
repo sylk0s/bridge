ac907d309fbd1daad1ad880ac5b64f2bbc4dd9a7
sylkos <sylkos49@gmail.com>
2022-12-31 13:27:20 -0500

    bridge app for mc-docker maybe works?

--- src/lib.rs
+++ src/lib.rs
@@ -11,17 +11,7 @@ pub struct Server {
     pub send_to: Vec<String>,
 }
 
-// Parses the string input into a tellraw command to send back to the server with /exec
-fn to_json(s: String) -> String {
-    unimplemented!();
-}
-
-// Send the command to the server with /exec
-fn send(s: String) {
-    unimplemented!();
-}
-
-// Determines if a message is a chat message or not
-fn is_chat(s: &String) {
-    unimplemented!();
+#[derive(Debug, Deserialize)]
+pub struct McdConf {
+    pub ws_port: u16,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
-use bridge_app::{Config, Server};
+use bridge_app::{Config, McdConf};
+use std::fs;
+use futures::StreamExt;
 
-fn main() {
-    // read and parse config
-    // TODO change config structure (when I implement it for the main webserver)
-     
-    // for each server, spawn a new thread sending the output to each of the listening servers
+const CONF_PATH: &str = "/home/sylkos/.config/mc-docker";
+
+#[tokio::main]
+async fn main() {
+    let mcd_file = fs::read_to_string(format!("{CONF_PATH}/config.toml")).expect("Failed to read mcd config"); 
+    let mcd_config: McdConf = toml::from_str(&mcd_file).expect("Could not parse config.toml");
+    
+    let config_file = fs::read_to_string(format!("{CONF_PATH}/bridge.json")).expect("Failed to read bridge.json");
+    let config: Config = serde_json::from_str(&config_file).expect("Could not parse bridge.json");
+
+    let path = format!("localhost:{}", mcd_config.ws_port);
+
+    for server in config.servers {
+        let p = path.clone();
+        tokio::spawn(async move {
+            let mut stream = reqwest::get(format!("{p}/cleanout/{}", server.name)).await.unwrap().bytes_stream();
+            while let Some(msg) = stream.next().await {
+                let msg = std::str::from_utf8(&msg.unwrap()).unwrap().to_string();
+                for destination in &server.send_to {
+                    let body = format!(r#"{{"args"["tellraw", "@a" "\"{{\"text\":\"{}\"}}\""]}}"#, msg);
+                reqwest::Client::new().post(format!("{p}/exec/{}", destination)).body(body)
+                    .send().await.expect("Failed to send command to server");
+                }
+            }
+        });
+    }
 }
